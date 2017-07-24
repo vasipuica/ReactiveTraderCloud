4929d4d5afbc4f99b38276b753669fb1e9ec3bf8
Vasi Puica <vasi.puica@gmail.com>
2017-07-24 15:32:00 +0100

    removed redux logger

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -5,19 +5,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { createLogger } from 'redux-logger'
-
 import autobahnConnection from './services/autobahn'
 import { MainContainer } from './ui/main'
 import rootReducer from './reducers'
 
-declare var process: any
-const env = process.env.NODE_ENV
 const middlewares: any[] = [thunk]
-
-if (env === 'dev') {
-  middlewares.push(createLogger())
-}
 
 const autobahnService = () => ({
   type: 'START_BOOTSTRAP',
