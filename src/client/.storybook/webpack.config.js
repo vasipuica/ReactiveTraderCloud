954ea94448aeecb1d403f10d8c88e9f31220bd6e
Pawel Misiurski <pawel@weareadaptive.com>
2017-07-24 15:30:25 +0100

    Storybook config

--- src/client/.storybook/webpack.config.js
+++ src/client/.storybook/webpack.config.js
@@ -6,13 +6,22 @@
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
 
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    loaders: [
-      // add your custom loaders.
-    ],
-  },
+// load the default config generator.
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+  // Extend it as you need.
+  // For example, add typescript loader:
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('ts-loader')
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: ['style-loader', 'css-loader', 'sass-loader']
+  });
+  
+  return config;
 };
