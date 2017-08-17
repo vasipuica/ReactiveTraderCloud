eff08d3486c59112ce2bb17849159094e9586c3b
Pawel Misiurski <pawel@weareadaptive.com>
2017-08-17 16:08:03 +0100

    chore: cherry pick from ff961000203b8b49909d971e83a7adfde8791a5a

--- src/client/src/system/schedulerService.ts
+++ src/client/src/system/schedulerService.ts
@@ -1,23 +1,23 @@
-import { Scheduler } from 'rxjs/Rx';
+import { Scheduler } from 'rxjs/Rx'
 
 /**
  * Abstracts scheduling concerns to enable testing
  */
 export default class SchedulerService {
 
-  _immediate: any;
-  _async: any;
+  _immediate: any
+  _async: any
 
   constructor() {
-    this._immediate = Scheduler.asap;
-    this._async = Scheduler.asap;
+    this._immediate = Scheduler.asap
+    this._async = Scheduler.asap
   }
 
   get immediate() {
-    return this._immediate;
+    return this._immediate
   }
 
   get async() {
-    return this._async;
+    return this._async
   }
 }
